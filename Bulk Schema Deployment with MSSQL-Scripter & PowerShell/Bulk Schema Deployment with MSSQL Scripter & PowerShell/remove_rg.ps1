b584bb77ab8427c8cec281e099d85b8ea2bec2c8
Arshad Ali <arali@microsoft.com>
2018-12-06 08:21:03 -0800

    Deleted unnecessary folders

--- Bulk Schema Deployment with MSSQL-Scripter & PowerShell/Bulk Schema Deployment with MSSQL Scripter & PowerShell/remove_rg.ps1
+++
@@ -1,9 +0,0 @@
-##########################################################################
-# Creator: Paula Berenguel - Jul/2017
-# This PowerShell script will remove a resource group named mlsresource,
-# How to run the script: & "C:\MyPS\remove_rg.ps1"
-############################################################################
-
-# ResourceGroup name
-$resourcegroupname = "mls_rg"
-Remove-AzureRmResourceGroup -ResourceGroupName $resourcegroupname
